3c1a140c6bebf9bed671a7f3707362e514dcb809
Fbada006 <ferdinandkamata@yahoo.com>
2021-05-12 21:11:31 +0300

    hook up everything to UI

--- app/src/main/java/com/example/adichallenge/MainActivity.kt
+++ app/src/main/java/com/example/adichallenge/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.adichallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.adichallenge.adapter.ProductAdapter
+import com.example.adichallenge.databinding.ActivityMainBinding
+import com.example.adichallenge.models.Product
+import com.example.adichallenge.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewmodel by viewModels<ProductViewModel>()
+    private lateinit var productAdapter: ProductAdapter
+    private var productList = mutableListOf<Product>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        productAdapter = ProductAdapter(productList)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewmodel.products.observe(this, { productRes ->
+            when (productRes) {
+                is Resource.Failure -> {
+                    showFailureUi()
+                }
+                is Resource.Loading -> {
+                    showLoadingUi()
+                }
+                is Resource.Success -> {
+                    showSuccessUi(productRes.data)
+                }
+            }
+        })
+    }
+
+    private fun showSuccessUi(data: List<Product>) {
+        binding.pbProductLoading.isVisible = false
+        binding.tvError.isVisible = false
+        binding.rvProduct.isVisible = true
+        productList.clear()
+        productList.addAll(data)
+        productAdapter.notifyDataSetChanged()
+    }
+
+    private fun showLoadingUi() {
+        binding.tvError.isVisible = false
+        binding.rvProduct.isVisible = false
+        binding.pbProductLoading.isVisible = true
+    }
+
+    private fun showFailureUi() {
+        binding.pbProductLoading.isVisible = false
+        binding.rvProduct.isVisible = false
+        binding.tvError.isVisible = true
     }
 }
